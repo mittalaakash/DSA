b06fbae92e8b95509f466937362cdcd5a5c1b528
Mridul <mit.ash.619@gmail.com>
2023-02-05 12:17:25 +0530

    function

---
+++ functions/functcion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printName()
+{
+    int n;
+    cout << "enter the value" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Mridul" << endl;
+    }
+};
+
+int main()
+{
+    printName();
+}
