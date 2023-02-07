c49399d3c4c5f836ec02ed7ffd477338e76d1ef6
Mridul <mit.ash.619@gmail.com>
2023-02-08 03:09:39 +0530

    pattern ++

---
+++ patterns/fancy pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+
+    for (int row = 0; row < size; row++)
+    {
+        for (int col = 0; col < 2 * row + 1; col++)
+        {
+            if ((col + 1) % 2 == 0)
+                cout << "*";
+            else
+                cout << row + 1;
+        }
+        cout << endl;
+    }
+    for (int row = 0; row < size; row++)
+    {
+        for (int col = 0; col < 2 * (size - row) - 1; col++)
+        {
+            if ((col + 1) % 2 == 0)
+                cout << "*";
+            else
+                cout << (size - row);
+        }
+        cout << endl;
+    }
+}
