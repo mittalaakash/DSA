1e7fee697cd7e1581ab982bde68baab9a08ee2e0
Mridul <mit.ash.619@gmail.com>
2023-02-18 01:55:27 +0530

    missing element

--- Searching/missing element.cpp
+++ Searching/missing element.cpp
@@ -2,18 +2,31 @@
 #include <vector>
 using namespace std;
 
-int missingElement(vector<int> v) {}
+int missingElement(vector<int> v)
+{
+    int start = 0;
+    int end = v.size() - 1;
+    int mid = start + (end - start) / 2;
+
+    while (start < end)
+    {
+        if (v[mid] == mid + 1)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return mid;
+}
 
 int main()
 {
     vector<int> v{1, 2, 3, 4, 6, 7};
     int value = missingElement(v);
-    cout << value;
-    // if ()
-    // {
-    //     cout << "found";
-    // }
-    // else
-    //     cout << "not found";
+
     return 0;
 }
