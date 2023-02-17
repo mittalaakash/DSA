57a6610ea6e7b81e683e2a35e4981837a4d4c39a
Mridul <mit.ash.619@gmail.com>
2023-02-18 01:45:22 +0530

    find peak of mountain

---
+++ Searching/missing element.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int missingElement(vector<int> v) {}
+
+int main()
+{
+    vector<int> v{1, 2, 3, 4, 6, 7};
+    int value = missingElement(v);
+    cout << value;
+    // if ()
+    // {
+    //     cout << "found";
+    // }
+    // else
+    //     cout << "not found";
+    return 0;
+}
